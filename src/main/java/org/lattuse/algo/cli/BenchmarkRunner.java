887f64fbe349674a95d188b1ba652e83bf5406c4
Lattuse <sultik280805@gmail.com>
2025-10-05 07:49:18 +0500

    github CI and becnhmark things

---
+++ src/main/java/org/lattuse/algo/cli/BenchmarkRunner.java
@@ -0,0 +1,10 @@
+package org.lattuse.algo.cli;
+
+import org.openjdk.jmh.Main;
+
+public class BenchmarkRunner {
+    public static void main(String[] args) throws Exception {
+        Main.main(args);
+    }
+}
+
